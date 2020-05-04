8e9273e44fbc849b44cd2294750e0d3f397c7178
zhenbang <zbchen29@gmail.com>
2020-05-03 21:05:25 -0400

    Add functions for requesting and saving images

--- collect_data.py
+++ collect_data.py
@@ -1,12 +1,19 @@
 import requests
 import json
+from pathlib import Path
+from time import sleep
 
-def get_ids():
-    '''Return a list of valid art object ids from the Met'''
+### Documentation for MET API: https://metmuseum.github.io/
 
-    met_endpoint = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+def get_ids(params={}):
+    '''Return a list of valid art object ids from the Met based on params'''
 
-    response_json = requests.get(met_endpoint).json()
+    if params:
+        met_endpoint = "https://collectionapi.metmuseum.org/public/collection/v1/search"
+    else:
+        met_endpoint = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+
+    response_json = requests.get(met_endpoint, params=params).json()
     return response_json["objectIDs"]
 
 def get_art(id):
@@ -21,6 +28,17 @@ def get_art(id):
     response_json = requests.get(met_endpoint + str(id)).json()
     return response_json
 
+def get_picture(image_url):
+    '''Return the request content of a request to an image
+    
+    Paramters
+        url (string) : the url of the image to retrieve
+    '''
+
+    request = requests.get(image_url)
+    return request.content
+
+
 def save_to_file(obj, filename):
     '''Saves an object to a local json file
 
@@ -28,7 +46,7 @@ def save_to_file(obj, filename):
         obj (object) : the object to save
         filename (string) : the filename to use
     '''
-    path = filename + ".json"
+    path = filename
     with open(path, 'w') as f:
         f.write(json.dumps(obj))
 
@@ -40,16 +58,64 @@ def load_from_file(filename):
     Parameters
         filename (string) : the filename to load from
     '''
-    path = filename + ".json"
+    path = filename
     obj = None
     with open(path) as f:
         obj = json.loads(f.read())
 
     return obj
-    
+
+def save_picture(path, filename, image_data):
+    '''Saves a picture to the specified location and filename
+    '''
+    # Make the directory if it does not exist
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    path = path + "/" + filename
+
+    with open(path, "wb") as f:
+        f.write(image_data)
+
+    # print("Saved image to", path)
+
+def get_century(year):
+    '''Returns the century of a specified year (negative for BC)'''
+    if year == 0:
+        # raise ValueError("Year cannot be zero!")
+        return 0
+    elif year > 0:
+        return ((year-1) // 100) + 1
+    else:
+        return (year // 100)
+
 def main():
-    # for id in get_ids()[:1000]:
-    save_to_file(get_ids(), "ids")
+
+    dept6_ids = load_from_file("dept6_ids.json")
+    test_arts = load_from_file("test_arts.json")
+
+    for index, id in enumerate(dept6_ids[1000:5000]):
+        if index % 100 == 0:
+            print(index)
+        sleep(0.1)
+        art = get_art(id)
+        test_arts.append(art)
+    save_to_file(test_arts, "test_arts.json")
+
+    # test_arts = load_from_file("test_arts.json")
+    # start_index = 1000
+    # for (index, art) in enumerate(test_arts):
+    #     if index % 50 == 0:
+    #         print(index)
+    #     sleep(0.1)
+
+    #     year = art["objectBeginDate"]
+    #     image_url = art["primaryImageSmall"]
+
+    #     century = str(get_century(year))
+    #     path = "data/" + century
+    #     filename = str(start_index + index) + ".jpg"
+
+    #     save_picture(path, filename, get_picture(image_url))
 
 if __name__ == "__main__":
     main()
